53604b72dcc84f60ac9a4c2faea4d122cac5d802
Varasina Farmadani <sina@sinanonym.my.id>
2026-05-27 07:27:42 +0700

    fix(test): drop file before rename for windows bench test

--- benches/file_write_bench.rs
+++ benches/file_write_bench.rs
@@ -1,22 +1,35 @@
 use criterion::{Criterion, criterion_group, criterion_main};
-use std::path::PathBuf;
+use std::{
+    fs,
+    io::Write,
+    path::{Path, PathBuf},
+};
 
-fn write_direct(path: &PathBuf, data: &[u8]) {
-    std::fs::write(path, data).unwrap();
+fn write_direct(path: &Path, data: &[u8]) {
+    fs::write(path, data).unwrap();
 }
 
-fn write_atomic(path: &PathBuf, data: &[u8]) {
-    let tmp = path.with_extension("tmp");
-    std::fs::write(&tmp, data).unwrap();
-    // fsync simulasi (flush)
-    let f = std::fs::File::open(&tmp).unwrap();
-    f.sync_all().unwrap();
-    std::fs::rename(&tmp, path).unwrap();
+fn write_atomic(path: &Path, data: &[u8]) {
+    let tmp = atomic_temp_path(path);
+    let _ = fs::remove_file(&tmp);
+
+    let mut file = fs::File::create(&tmp).unwrap();
+    file.write_all(data).unwrap();
+    file.sync_all().unwrap();
+    drop(file);
+    #[cfg(windows)]
+    let _ = fs::remove_file(path);
+
+    fs::rename(&tmp, path).unwrap();
+}
+
+fn atomic_temp_path(path: &Path) -> PathBuf {
+    path.with_extension(format!("tmp-{}", std::process::id()))
 }
 
 fn bench_file_write(c: &mut Criterion) {
     let data = vec![0u8; 4096]; // ukuran tipikal state file
-    let path = std::env::temp_dir().join("etle_bench_write.bin");
+    let path = std::env::temp_dir().join(format!("etle_bench_write_{}.bin", std::process::id()));
     let mut group = c.benchmark_group("file_write");
 
     group.bench_function("fs_write_direct", |b| b.iter(|| write_direct(&path, &data)));
@@ -28,7 +41,8 @@ fn bench_file_write(c: &mut Criterion) {
     group.finish();
 
     // Bersihkan file sementara
-    let _ = std::fs::remove_file(&path);
+    let _ = fs::remove_file(&path);
+    let _ = fs::remove_file(atomic_temp_path(&path));
 }
 
 criterion_group!(benches, bench_file_write);
